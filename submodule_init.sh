fcb602dc02727d5d173442494fd78e71a539aeb1
wenbin <wenbin.shi@gmail.com>
2022-09-11 12:23:51 +0800

    fix bug

--- submodule_init.sh
+++ submodule_init.sh
@@ -5,3 +5,7 @@ git submodule update --init --recursive
 
 ## If meet the buleyes invalid signatrur error,run the
 ## docker system prune
+
+
+## start more service
+## ./test-node.bash --init --validate --redundantsequencers 3
