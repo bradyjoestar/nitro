81d59028d911fd2809d4a23f4fda2a8d290f22ad
wenbin <wenbin.shi@gmail.com>
2022-09-13 10:51:37 +0800

    add useful scripts

--- submodule_init.sh
+++ submodule_init.sh
@@ -9,3 +9,13 @@ git submodule update --init --recursive
 
 ## start more service
 ## ./test-node.bash --init --validate --redundantsequencers 3
+
+## private key: e887f7d17d07cc7b8004053fb8826f6657084e88904bb61590e498ca04704cf2
+## address : 0x683642c22feDE752415D4793832Ab75EFdF6223c
+## l1 balance: 1.158E+59 Ether
+## l2 balance: 101,000 Ether
+
+
+
+## L2 precompile contracts
+## https://github.com/OffchainLabs/nitro/blob/8c36adc97bbb0c3a5ff95b5bd1b18f074a4d25fc/docs/useful-addresses.md
